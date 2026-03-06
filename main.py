d91aefc0df3cc5265e0be6ae2ceeae9297b75f6e
HoualefAkram <akram1232004@gmail.com>
2026-03-06 05:57:06 +0100

    added render class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from user_equipment import UserEquipment
 from base_tower import BaseTower
 from latlng import LatLng
 from wave_utils import WaveUtils
+from render import Render
 
 
 bs1 = BaseTower(
@@ -30,27 +31,32 @@ car = UserEquipment(
     serving_bs=bs2,  # starts connected to bs2
 )
 bs2.add_ue(ue=car)
+ue_path = [car.latlng]
 
 
 rsrp1 = WaveUtils.calculate_rsrp(ue=car, bs=bs2)
 print(f"rsrp1: {rsrp1:.2f} dBm")
 
-car.move_meters(68, angle=270)
-
+car.move_meters(70, angle=270)
+ue_path.append(car.latlng)
 
 rsrp2 = WaveUtils.calculate_rsrp(ue=car, bs=bs2)
 print(f"rsrp2: {rsrp2:.2f} dBm")
 
 
-car.move_meters(56, angle=320)
+car.move_meters(56, angle=340)
+ue_path.append(car.latlng)
 
 rsrp3 = WaveUtils.calculate_rsrp(ue=car, bs=bs2)
 print(f"rsrp3: {rsrp3:.2f} dBm")
 
-car.move_meters(200, angle=270)
+car.move_meters(200, angle=275)
+ue_path.append(car.latlng)
 
 
 rsrp4 = WaveUtils.calculate_rsrp(ue=car, bs=bs2)
 print(f"rsrp4: {rsrp4:.2f} dBm")
 
 print(car.generate_report(all_bs=[bs1, bs2, bs3]))
+
+Render.render_map(bs_list=[bs1, bs2, bs3], ue=car, ue_path=ue_path)
---
+++ render.py
@@ -0,0 +1,37 @@
+import folium
+from base_tower import BaseTower
+from user_equipment import UserEquipment
+
+
+class Render:
+    @staticmethod
+    def render_map(bs_list: list[BaseTower], ue: UserEquipment, ue_path: list = []):
+        m = folium.Map(
+            location=[bs_list[0].latlng.lat, bs_list[0].latlng.long], zoom_start=16
+        )
+
+        # Base Towers
+        for bs in bs_list:
+            folium.Marker(
+                location=[bs.latlng.lat, bs.latlng.long],
+                tooltip=f"BS {bs.id}",
+                popup=f"BS{bs.id} | {bs.frequency/1e9:.1f}GHz | {bs.p_tx}dBm",
+                icon=folium.Icon(color="red", icon="tower-cell", prefix="fa"),
+            ).add_to(m)
+
+        # UE path
+        if len(ue_path) > 1:
+            folium.PolyLine(
+                locations=[[p.lat, p.long] for p in ue_path],
+                color="blue",
+                weight=2,
+            ).add_to(m)
+
+        # UE current position
+        folium.Marker(
+            location=[ue.latlng.lat, ue.latlng.long],
+            tooltip=f"UE {ue.id}",
+            icon=folium.Icon(color="blue", icon="car", prefix="fa"),
+        ).add_to(m)
+
+        m.save("simulation.html")
