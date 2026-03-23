ba0e96be2ee07f91f30648c65a4088c758aab52e
HoualefAkram <akram1232004@gmail.com>
2026-03-23 02:34:32 +0100

    added space and action spaces

--- rl/handover_env.py
+++ rl/handover_env.py
@@ -1,10 +1,13 @@
 import gymnasium as gym
+from gymnasium.spaces import Discrete
 
 
 class HandoverEnv(gym.Env):
 
     def __init__(self):
         super().__init__()
+        self.observation_space = Discrete(12)
+        self.action_space = Discrete(4)
 
     def step(self, action):
         return super().step(action)
