de3049f061a487a58ce658406929f195a586aea8
HoualefAkram <akram1232004@gmail.com>
2026-03-29 21:58:46 +0100

    change simulation location

--- prepare.py
+++ prepare.py
@@ -7,8 +7,8 @@ from utils.path_gen import PathGeneration
 from utils.tower_downloader import TowerDownloader
 
 # --- Params ---
-MAP_TOP_LEFT = LatLng(51.519411, -0.148076)  # London
-MAP_BOTTOM_RIGHT = LatLng(51.499324, -0.109732)  # London
+MAP_TOP_LEFT = LatLng(52.040089, -0.774654)  # UK
+MAP_BOTTOM_RIGHT = LatLng(52.035549, -0.735773)  # UK
 MCC = 234  # UK
 OSM_DOWNLOAD_PATH = "cache/maps/map.osm"  # cache folder
 SIMULATION_TIME = 300  # (5 minutes)
